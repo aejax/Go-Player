2307d675221454dfa7ef03206831aa1d36366da0
Nathan Cannon <nathan.r.cannon@gmail.com>
2015-07-17 18:19:44 -0500

    Added GtpEngine code from Fuego

--- GoPlayer/main.cpp
+++ GoPlayer/main.cpp
@@ -3,4 +3,6 @@ I want to be able to interface with the GoGui using the GTP(Go text protocol).
 I think that I will base this interface on the one created for Fuego.
 
 I also managed to sync this project with GitHub.
+
+GTP reguires a controller and an engine. My code must serve as the engine.
 */
