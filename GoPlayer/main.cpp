829d42489e0344120e100fa31ca9a4e338803930
Nathan Cannon <nathan.r.cannon@gmail.com>
2015-07-16 19:37:36 -0500

    Project goals commented in main

---
+++ GoPlayer/main.cpp
@@ -0,0 +1,6 @@
+/*
+I want to be able to interface with the GoGui using the GTP(Go text protocol).
+I think that I will base this interface on the one created for Fuego.
+
+I also managed to sync this project with GitHub.
+*/
